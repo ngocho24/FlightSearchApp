b977285c5059f1268a1bdd8840b32b5a9d39ae6d
Elijah Ngocho <elijahngocho24@gmail.com>
2025-07-10 12:07:23 +0300

    Navigation confirmed

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native';
+import { Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Login Screen</Text>
+      <Button 
+        title="Go to Signup" 
+        onPress={() => navigation.navigate('Signup')} 
+      />
     </View>
   );
 }
